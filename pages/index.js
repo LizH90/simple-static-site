5f6666c6ede7068f6aacf5fe155bd6865118f9d1
LizH90 <elizabeth_hard@hotmail.co.uk>
2021-01-20 18:53:30 +0000

    Update homepage content

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <h1>Andy & Lizzie</h1>
+        <p>July 9th 2022, Hayne, Devon</p>
+      </div>
+      <div className={styles.image}>
+        <Image 
+          src={'/../public/flowers.jpg'}
+          height={600}
+          width={1100}
+          responsive='responsive'
+        />
+      </div>
+      <div className={styles.imageWrapper}>
+        <Image 
+        className={styles.circle}
+        src={'/../public/cottage.jpg'}
+        height={400}
+        width={400}
+      />
+      </div>
+      <div className={styles.timetable}>
+        <h3>Running order of the day</h3>
+        <strong>Friday</strong>
+        <p>BBQ for happy campers</p>
+        <strong>Saturday</strong>
+        <p>Bar open 12.30pm</p>
+        <p>Service 2pm</p>
+        <p>Canapés and Lawn Games 3pm</p>
+        <p> Wedding Breakfast 5pm</p>
+        <p>Cut the cake</p>
+        <p>Firepit and Dancing</p>
+        <p>Carriages 1am</p>
+        <strong>Sunday</strong>
+        <p>Breakfast for campers</p>
+      </div>
+      <div className={styles.details}>Dress code: Smart Casual</div>
+      <div className={styles.details}>Car park on site</div>
+      <div className={styles.details}> Cars can be picked up the next day</div>
+    </>
   )
 }
