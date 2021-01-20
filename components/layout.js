b9606cfa0ace8f87c96bb8acd6dbfa787f47b02b
LizH90 <elizabeth_hard@hotmail.co.uk>
2021-01-20 18:53:01 +0000

    Add nav in Layout

---
+++ components/layout.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import styles from '../styles/Layout.module.css';
+
+const Layout = ({children}) => (
+  <>
+    <nav>
+      <div>
+        <Link href="/">
+          <a className={styles.navlink}>Home</a>
+        </Link>
+      </div>
+      <div >
+        <Link href="/wedding">
+          <a className={styles.navlink}>Wedding</a>
+        </Link>
+        <Link href="/location">
+          <a className={styles.navlink}>Location</a>
+        </Link>
+        <Link href="/accomodation">
+          <a className={styles.navlink}>Accom</a>
+        </Link>
+      </div>
+    </nav>
+    {children}
+  </>
+);
+
+export default Layout;
